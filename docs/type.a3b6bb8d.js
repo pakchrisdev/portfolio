812685211fa27eeba89d4f8b83ed410d9daa8339
christopher pak <macbookpro@christophersMBP.fios-router.home>
2019-06-15 05:51:15 -0400

    t v1.4

--- docs/type.a3b6bb8d.js
+++ docs/type.a3b6bb8d.js
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62212" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62259" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -383,4 +383,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","scripts/type.js"], null)
-//# sourceMappingURL=/type.a3b6bb8d.js.map
+//# sourceMappingURL=type.a3b6bb8d.js.map
